ce3f43c042a5592445f05ff074f718a17054ca81
Imen Zighed <zighedimen921@gmail.com>
2021-03-08 20:52:16 +0100

    context

---
+++ src/components/DetailsThumb.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function DetailsThumb({images, setIndex}) {
+    return (
+        <div className="thumb">
+            {
+               images.map((img, index)=>(
+                <img  src={img} alt="" key={index} onClick={() => setIndex(index)}/> 
+                ))
+            }
+    </div>
+    )
+}
