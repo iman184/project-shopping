c3acfbf9b9ba756302ce7fc0bbb83618a9c7a339
Imen Zighed <zighedimen921@gmail.com>
2021-03-08 20:52:34 +0100

    cart / app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Header from './components/Header'
+import Products from './components/Products'
+import Details from './components/Details'
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import {DataProvider} from './components/DataProvider'
+import Cart from './components/Cart'
+class App extends React.Component{
+  render(){
+    return(
+      <DataProvider>
+     
+        <div classsName="App">
+         
+          <Router>
+         <Header />
+       <section>
+       <Switch>
+        <Route  path='/products' component={Products} />
+        <Route  path='/details/:id' component={Details} />
+        <Route path='/cart'  component={Cart} />
+        </Switch>
+        </section>
+        </Router>
+        </div> 
+        
+        
+    </DataProvider>
+    );
+  }
 }
 
 export default App;
